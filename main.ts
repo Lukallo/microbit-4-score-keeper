480d76052293811bebbe0dd485aa11e3473e951f
Lukallo <103218481+Lukallo@users.noreply.github.com>
2024-11-12 10:50:13 +0000

    Shake function

--- main.ts
+++ main.ts
@@ -35,5 +35,12 @@ input.onButtonPressed(Button.B, function () {
     basic.pause(2000)
     basic.clearScreen()
 })
-let Score_A = 0
+input.onGesture(Gesture.Shake, function () {
+    basic.showNumber(Score_A)
+    basic.pause(2000)
+    basic.showNumber(Score_B)
+})
 let Score_B = 0
+let Score_A = 0
+Score_A = 0
+Score_B = 0
